63147b85ff0b8faecca427990d08d9bee2319351
ShivayAnit <byankitsharma@gmail.com>
2023-08-25 22:32:03 +0530

    added prime number code

---
+++ prime.js
@@ -0,0 +1,16 @@
+let num = 5;
+ let fact = 0;
+  for(let i = 1; i < num; i++)
+  {
+    if(num % i == 0)
+    {
+        fact++;
+    }
+  }
+  if(fact == 2)
+  {
+    console.log("Prime");
+  }
+  else{
+    console.log("Not prime")
+  }
